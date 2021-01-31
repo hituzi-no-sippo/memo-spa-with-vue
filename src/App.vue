d89c7c5502fc44b41a80eeeabd0fd2385af63991
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-01-31 17:55:24 +0900

    feat: dummyのlistを作成

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TitleList :contents="contents" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TitleList from './views/TitleList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TitleList
+  },
+  setup() {
+    const contents = [
+      {title: 'aa', body: 'bb'},
+      {title: 'cc', body: 'dd'}
+    ]
+
+    return { contents }
   }
 }
 </script>
---
+++ src/views/TitleList.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul>
+    <li v-for="(content, index) in contents" :key="index">
+      {{ content.title }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'TitleList',
+  props: {
+    contents: Array
+  },
+}
+</script>
