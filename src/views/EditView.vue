6c12610ea2116247400fd972dd6ef1e8ae965299
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-02-04 20:22:50 +0900

    feat: EditViewのcssを作成

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -1,15 +1,88 @@
 <template>
-  <div>
-    <input v-model="content.title" />
-    <textarea v-model="content.body" />
-    <span v-if="isEmptyTitle">文字を入力してください</span>
-    <button
-      :disabled="isEmptyTitle"
-      @click="update"
+  <div class="
+    flex
+    flex-col
+    m-4
+    p-2
+    border-green-400
+    border-2
+  ">
+    <input
+      v-model="content.title"
+      class="
+        text-2xl
+        fond-bold
+        border
+        border-black
+        focus:outline-none
+        focus:ring-black
+        focus:ring-2
+        px-2
+        my-2
+        rounded-full
+      "
+    />
+    <textarea
+      v-model="content.body"
+      class="
+        text-2xl
+        fond-bold
+        border
+        border-black
+        focus:outline-none
+        focus:ring-black
+        focus:ring-1
+        px-2
+        py-1
+        my-4
+        h-full
+      "
+    />
+    <span
+      v-if="isEmptyTitle"
+      class="
+        text-red-500
+        font-bold
+        text-center
+      "
+    >文字を入力してください</span>
+    <div
+      class="
+        buttons
+        my-2
+        text-center
+      "
     >
-      Update
-    </button>
-    <button @click="remove">Delete</button>
+      <button
+        :disabled="isEmptyTitle"
+        @click="update"
+        class="
+          bg-green-600
+          hover:bg-green-500
+          font-semibold
+          text-white
+          rounded-full
+          px-4
+          mx-3
+          disabled:opacity-50
+          disabled:cursor-not-allowed
+        "
+      >
+        Update
+      </button>
+      <button
+        @click="remove"
+        class="
+          bg-green-600
+          hover:bg-green-500
+          font-semibold
+          text-white
+          rounded-full
+          px-4
+          mx-3
+        "
+      >Delete</button>
+    </div>
   </div>
 </template>
 
